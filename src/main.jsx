95b1068d71fa0300d537c7fc3f29419231cb5438
imanisul <ianisul768@gmail.com>
2024-03-09 00:57:00 +0530

    added basic redux setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,18 @@
-import ReactDOM from 'react-dom/client'
+import './index.css';
+
+import ReactDOM from 'react-dom/client';
+import {Toaster} from 'react-hot-toast';
+import { Provider } from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+
 import App from './App.jsx'
-import './index.css'
+import store from './Redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
+  <Provider store={store}>
+  <BrowserRouter>
     <App />
-)
+    <Toaster/>
+</BrowserRouter>
+</Provider>
+);
