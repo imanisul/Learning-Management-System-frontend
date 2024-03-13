264403200e1cee5350cba1bd595c7fe32ad0bc66
imanisul <ianisul768@gmail.com>
2024-03-13 17:23:46 +0530

    Added courses page flow

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import {  Route, Routes } from 'react-router-dom'
 
 import AboutUsPage from './Pages/AboutUsPage.jsx'
+import CourseList from './Pages/Course/CourseList.jsx'
 import HomePage from './Pages/HomePage.jsx'
 import Login from './Pages/Login.jsx'
 import NotFound from './Pages/NotFound.jsx'
@@ -12,12 +13,13 @@ import SignUp from './Pages/SignUp.jsx'
 
 function App() {
  
-
+    
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />}/>
         <Route path="/about" element={<AboutUsPage />}/>
+        <Route path='/courses' element= {<CourseList/>}/>   
 
         <Route path='/signup' element={<SignUp/>}/>
         <Route path='/login' element={<Login/>}/>
